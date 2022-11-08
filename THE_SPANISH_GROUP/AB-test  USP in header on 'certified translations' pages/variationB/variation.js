025b5e4378d2429765f24a91880b778a7e828d0d
deven <devenmer@gmail.com>
2022-11-08 18:27:17 +0530

    code

--- THE_SPANISH_GROUP/AB-test  USP in header on 'certified translations' pages/variationB/variation.js
+++ THE_SPANISH_GROUP/AB-test  USP in header on 'certified translations' pages/variationB/variation.js
@@ -26,19 +26,20 @@
   
     let htmlStr = `<h3 class="eg_tile">Translating Over 90 Languages</h3>`
     let htmlStr2 = `<h3 class="eg_tile"> <span class="eg_one">Lightning Fast Delivery</span> <br/> <span class="eg_span">Same Day Delivery If Needed</span></h3>`
+    let htmlStr3 = `<h3 class="eg_tile"> <span class="eg_one">100% Guaranteed Accepted Translated Documents</span></h3>`
 
 function init(){
 
 
-  if (window.location.href.indexOf('/immigration-certificate-translation/') != -1) {
+  if (window.location.href.indexOf('https://thespanishgroup.org/translations/diploma-and-transcript-spanish-translation/') != -1) {
     document.querySelector("#header-img > section.template_banner .row div").insertAdjacentHTML("afterend", htmlStr);
   } 
-  else if (window.location.href.indexOf('/certified-translations-for-official-civil-documents/') != -1) {
+  else if (window.location.href.indexOf('https://thespanishgroup.org/translations/certified-translations-for-official-civil-documents/') != -1) {
     document.querySelector("#header-img > section.template_banner .row div").insertAdjacentHTML("afterend", htmlStr2);
   } 
-  // else if (window.location.href.indexOf('/sitemap/') != -1) {
-  //   document.querySelector("#header-img > section.template_banner .row div").insertAdjacentHTML("afterend", htmlStr2);
-  // } 
+  else if (window.location.href.indexOf('https://thespanishgroup.org/translations/immigration-certificate-translation/') != -1) {
+     document.querySelector("#header-img > section.template_banner .row div").insertAdjacentHTML("afterend", htmlStr3);
+   } 
 }
     /* Initialize variation */
     waitForElement("#header-img > section.template_banner .row div", init, 50, 15000);
